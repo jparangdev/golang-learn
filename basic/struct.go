aaafdb72b7e0f76005590961e2848b7dc6d39012
jparangdev <jparangdev@gmail.com>
2024-01-02 22:48:14 +0900

    struct

---
+++ basic/struct.go
@@ -0,0 +1,66 @@
+package basic
+
+import (
+	"fmt"
+)
+
+// House struct type, with the properties Address (string), Size (integer),
+// Price (float64) and Type (string).
+type House struct {
+	Address string
+	Size    int
+	Price   float64
+	Type    string
+}
+
+// User struct type, with the properties Name (string), ID (string) and Age (integer).
+type User struct {
+	Name string
+	ID   string
+	Age  int
+}
+
+// VIPUser struct type. It embeds the User struct, and has additional properties
+// VIPLevel (integer) and Price (integer).
+type VIPUser struct {
+	User
+	VIPLevel int
+	Price    int
+}
+
+func StructExample() {
+
+	// Declaring a variable 'house' of type House
+	var house House
+	// Assigning values to the properties of 'house'
+	house.Address = "seoul, south korea"
+	house.Type = "apartment"
+	house.Size = 28
+	house.Price = 9.8
+
+	// Print the properties of 'house'
+	fmt.Println("주소:", house.Address)
+	fmt.Printf("크기: %d평\n", house.Size)
+	fmt.Println("가격: %.2f억 원\n", house.Price)
+	fmt.Println("타입:", house.Type)
+
+	// Initializing a variable of type User
+	user := User{"송하나", "hana", 23}
+	// Initializing a variable of type VIPUser
+	vip := VIPUser{
+		User{"화랑", "hawrang", 40},
+		3,
+		250,
+	}
+
+	// Print the properties of 'user'
+	fmt.Println("User: %s ID: %s Age: %d\n", user.Name, user.ID, user.Age)
+	// Print the properties of 'vip'
+	fmt.Println("VIP User: %s ID: %s Age: %d VIP Lv: %d VIP price : %d\n",
+		vip.Name,
+		vip.ID,
+		vip.Age,
+		vip.VIPLevel,
+		vip.Price,
+	)
+}
--- main/main.go
+++ main/main.go
@@ -29,4 +29,6 @@ func main() {
 	fmt.Println("======================================")
 	basic.ArrayExample()
 	fmt.Println("======================================")
+	basic.StructExample()
+	fmt.Println("======================================")
 }
