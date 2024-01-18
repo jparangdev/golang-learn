9b718ab3d96c3b7a56e264aaa009c575b52c8234
jparangdev <jparangdev@gmail.com>
2024-01-18 23:10:36 +0900

    slice.go part 2

--- basic/slice.go
+++ basic/slice.go
@@ -21,5 +21,28 @@ func SliceExample() {
 
 	slice = append(slice, 11, 12, 13, 14, 15)
 	fmt.Println(slice)
+	fmt.Println(cap(slice))
+
+	slice1 := make([]int, 3, 5)
+	slice2 := append(slice1, 4, 5)
+	fmt.Println("slice1:", slice1, len(slice1), cap(slice1))
+	fmt.Println("slice2:", slice2, len(slice2), cap(slice2))
+
+	slice1[1] = 100
+	fmt.Println("after change")
+	fmt.Println("slice1:", slice1, len(slice1), cap(slice1))
+	fmt.Println("slice2:", slice2, len(slice2), cap(slice2))
+
+	slice1 = append(slice1, 500)
+	fmt.Println("after add")
+	fmt.Println("slice1:", slice1, len(slice1), cap(slice1))
+	fmt.Println("slice2:", slice2, len(slice2), cap(slice2))
+
+	slice3 := append(slice1, 99, 98, 97)
+	slice1[1] = 200
+	fmt.Println("after over cap")
+	fmt.Println("slice1:", slice1, len(slice1), cap(slice1))
+	fmt.Println("slice2:", slice2, len(slice2), cap(slice2))
+	fmt.Println("slice3:", slice3, len(slice3), cap(slice3))
 
 }
