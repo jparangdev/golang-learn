aeb50c4cd55a8cb227ebfe2235b317b851ec3832
jparangdev <jparangdev@gmail.com>
2024-01-23 22:51:01 +0900

    slice.go part 4

--- basic/slicing.go
+++ basic/slicing.go
@@ -1,6 +1,20 @@
 package basic
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
+
+type Student struct {
+	Name string
+	Age  int
+}
+
+type Students []Student
+
+func (s Students) Len() int           { return len(s) }
+func (s Students) Less(i, j int) bool { return s[i].Age < s[j].Age }
+func (s Students) Swap(i, j int)      { s[i], s[j] = s[j], s[i] }
 
 func SlicingExample() {
 	array := [5]int{1, 2, 3, 4, 5}
@@ -36,8 +50,32 @@ func SlicingExample() {
 
 	fmt.Println(cnt1, slice3)
 
+	fmt.Println("Remove an element at a index")
 	slice4 := []int{1, 2, 3, 4, 5, 6}
 	idx := 2
 	slice5 := append(slice4[:idx], slice4[idx+1:]...)
 	fmt.Println(slice5)
+
+	fmt.Println("Add an element at a index")
+	slice6 := append(slice5[:idx], append([]int{100}, slice5[idx:]...)...)
+	fmt.Println(slice6)
+
+	fmt.Println("Sort Ints")
+	sort.Ints(slice6)
+	fmt.Println(slice6)
+
+	s := []Student{
+		{Name: "John", Age: 18},
+		{Name: "Jane", Age: 20},
+		{Name: "Mark", Age: 19},
+		{Name: "Lisa", Age: 17},
+		{Name: "Bob", Age: 21},
+	}
+
+	sort.Sort(Students(s))
+
+	fmt.Println("Sorted Students by Age")
+	for _, student := range s {
+		fmt.Println(student.Name, student.Age)
+	}
 }
