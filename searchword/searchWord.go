54b80689755d397abed0053761277923826e6b61
jparangdev <jparangdev@gmail.com>
2024-02-26 22:21:50 +0900

    searchWord.go - part 1

---
+++ searchword/searchWord.go
@@ -0,0 +1,28 @@
+package searchword
+
+import (
+	"fmt"
+	"path/filepath"
+)
+
+func run() {
+
+}
+
+func GetFileList(path string) ([]string, error) {
+	return filepath.Glob(path)
+}
+
+func PrintAllFiles(files []string) {
+	for _, path := range files {
+		filelist, err := GetFileList(path)
+		if err != nil {
+			fmt.Println("it is wrong path, err:", err, " path:", path)
+			continue
+		}
+		fmt.Println("file list")
+		for _, name := range filelist {
+			fmt.Println(name)
+		}
+	}
+}
