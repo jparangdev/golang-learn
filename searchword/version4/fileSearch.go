be13b4b60a30c7bff4cea50813de11f7eda02587
jparangdev <jparangdev@gmail.com>
2024-03-06 23:24:33 +0900

    version 4

---
+++ searchword/version4/fileSearch.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+type LineInfo struct {
+	lineNo int
+	line   string
+}
+
+type FindInfo struct {
+	filename string
+	lines    []LineInfo
+}
+
+func main() {
+	if len(os.Args) < 3 {
+		fmt.Println("A minimum of 2 arguments are required")
+		return
+	}
+
+	word := os.Args[1]
+	files := os.Args[2:]
+	findInfos := []FindInfo{}
+	for _, path := range files {
+		findInfos = append(findInfos, FindWordInAllFiles(word, path)...)
+	}
+	for _, findInfo := range findInfos {
+		fmt.Println(findInfo.filename)
+		fmt.Println("------------------------------------")
+		for _, lineInfo := range findInfo.lines {
+			fmt.Println("\t", lineInfo.lineNo, "\t", lineInfo.line)
+		}
+		fmt.Println("------------------------------------")
+		fmt.Println()
+	}
+}
+
+func FindWordInAllFiles(word, path string) []FindInfo {
+	findInfos := []FindInfo{}
+
+	filelist, err := filepath.Glob(path)
+	if err != nil {
+		fmt.Println("Error getting file list:", err)
+		return findInfos
+	}
+	ch := make(chan FindInfo)
+	cnt := len(filelist)
+	recvCnt := 0
+
+	for _, filename := range filelist {
+		go FindWordInFile(word, filename, ch)
+	}
+
+	for findInfo := range ch {
+		findInfos = append(findInfos, findInfo)
+		recvCnt++
+		if recvCnt == cnt {
+			break
+		}
+	}
+	return findInfos
+}
+
+func FindWordInFile(word, filename string, ch chan FindInfo) {
+	findInfo := FindInfo{filename, []LineInfo{}}
+	file, err := os.Open(filename)
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		ch <- findInfo
+		return
+	}
+	defer file.Close()
+
+	lineNo := 1
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.Contains(line, word) {
+			findInfo.lines = append(findInfo.lines, LineInfo{lineNo, line})
+		}
+		lineNo++
+	}
+	ch <- findInfo
+}
---
+++ searchword/version4/go.mod
@@ -0,0 +1,3 @@
+module searchword/version4
+
+go 1.22rc1
