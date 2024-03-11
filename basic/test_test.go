721bbbaf1a7a18a4386ac0c8a27044b7ee8b3802
jparangdev <jparangdev@gmail.com>
2024-03-11 23:07:39 +0900

    test.go - part 2

--- basic/test_test.go
+++ basic/test_test.go
@@ -1,10 +1,23 @@
 package basic
 
-import "testing"
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
 
 func TestSquared1(t *testing.T) {
 	rst := squareTest(9)
 	if rst != 81 {
 		t.Errorf("fail test!")
 	}
+}
+
+func TestSquared2(t *testing.T) {
+	ass := assert.New(t)
+	ass.Equal(81, squareTest(9), "square(9) should be 81")
+}
+
+func TestSquared3(t *testing.T) {
+	ass := assert.New(t)
+	ass.Equal(9, squareTest(3), "square(3) should be 9")
 }
--- go.mod
+++ go.mod
@@ -1,1 +1,10 @@
 module golang-learn
+
+go 1.22rc1
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/testify v1.9.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
