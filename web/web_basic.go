c672c68deb8905f58cd7880f559478cfcc119442
jparangdev <jparangdev@gmail.com>
2024-03-27 21:54:35 +0900

    web part 3

--- web/web_basic.go
+++ web/web_basic.go
@@ -1,10 +1,25 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"strconv"
 )
+
+type Student struct {
+	Name  string
+	Age   int
+	Score int
+}
+
+func StudentHandler(w http.ResponseWriter, r *http.Request) {
+	var student = Student{"jude", 35, 95}
+	data, _ := json.Marshal(student)
+	w.Header().Add("content-type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, string(data))
+}
 
 func BarHandler(w http.ResponseWriter, r *http.Request) {
 	values := r.URL.Query()
@@ -19,6 +34,7 @@ func BarHandler(w http.ResponseWriter, r *http.Request) {
 func MakeWebHandler() http.Handler {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/bar", BarHandler)
+	mux.HandleFunc("/student", StudentHandler)
 	mux.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 	return mux
 }
--- web/web_basic_test.go
+++ web/web_basic_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"github.com/stretchr/testify/assert"
 	"net/http"
 	"net/http/httptest"
@@ -19,4 +20,21 @@ func TestBarHandler(t *testing.T) {
 	ast.Equal(http.StatusOK, res.Code)
 	expected := "Hello test, your ID is 1"
 	ast.Equal(expected, res.Body.String())
+}
+
+func TestJsonHandler(t *testing.T) {
+	ast := assert.New(t)
+
+	res := httptest.NewRecorder()
+	req := httptest.NewRequest("GET", "/student", nil)
+
+	mux := MakeWebHandler()
+	mux.ServeHTTP(res, req)
+
+	ast.Equal(http.StatusOK, res.Code)
+	student := new(Student)
+	err := json.NewDecoder(res.Body).Decode(student)
+	ast.Nil(err)
+	expected := &Student{"jude", 35, 95}
+	ast.Equal(expected, student)
 }
